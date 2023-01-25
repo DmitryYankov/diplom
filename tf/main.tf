d0132eb840c06eae405535084bce4ff66ef2a167
Dmitry Yankov <dmitryyankou@gmail.com>
2023-01-25 12:29:40 +0300

    6

--- tf/main.tf
+++ tf/main.tf
@@ -35,11 +35,10 @@ resource "aws_instance" "diplom" {
     owner = "Dmitry Yankov"
     }
   }
-# ### Add SSH-key
-# resource "aws_key_pair" "terraform_ec2_key" {
-# 	key_name = var.key_name
-# 	public_key = "${file("terraform_ec2_key.pub")}"
-# }
+### Add SSH-key
+resource "aws_key_pair" "terraform_ec2_key" {
+	key_name = var.key_name
+}
 
 ### Create security group
 resource "aws_security_group" "web" {
